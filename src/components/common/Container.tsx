24bb6f599b4d895b29006145e9fda3d6c51668d2
bojack <xlk970115@gmail.com>
2022-09-04 16:29:58 +0800

    add header

---
+++ src/components/common/Container.tsx
@@ -0,0 +1,10 @@
+import { ComponentProps, FC } from "react";
+
+export const Container: FC<ComponentProps<"div">> = ({
+  className,
+  ...props
+}) => {
+  return (
+    <div className={`w-full max-w-[1140px] mx-auto ${className}`} {...props} />
+  );
+};
--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
 import { Wallets } from "../Wallets";
+import { Container } from "./Container";
 
 const ROUTES = [
   {
@@ -22,18 +23,20 @@ const ROUTES = [
 
 export const Header = () => {
   return (
-    <div>
-      <div>DeFi</div>
-      <div>
-        {ROUTES.map((route) => (
-          <Link to={route.to} key={route.name}>
-            {route.name}
-          </Link>
-        ))}
-      </div>
-      <div>
-        <Wallets />
-      </div>
+    <div className="h-[64px] max-auto flex items-center justify-center border-b border-b-white/10">
+      <Container className="flex items-center justify-between">
+        <div className="text-[22px] font-bold">DeFiLogo</div>
+        <div className="flex items-center space-x-[30px]">
+          {ROUTES.map((route) => (
+            <Link to={route.to} key={route.name} className="font-semibold">
+              {route.name}
+            </Link>
+          ))}
+        </div>
+        <div>
+          <Wallets />
+        </div>
+      </Container>
     </div>
   );
 };
