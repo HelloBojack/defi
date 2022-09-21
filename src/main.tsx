f653462ac29b65bae9900cb94b5f20eff34cde61
bojack <xlk970115@gmail.com>
2022-09-21 23:26:32 +0800

    change useWeb3React

--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,13 @@ import "antd/dist/antd.dark.css";
 import "./style/index.css";
 import { store } from "./store";
 import { Provider } from "react-redux";
+import { Web3ReactProvider } from "@web3-react/core";
+import { connectors } from "./utils/wallet";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <Provider store={store}>
-    <App />
-  </Provider>
+  <Web3ReactProvider connectors={connectors}>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </Web3ReactProvider>
 );
--- src/utils/wallet.ts
+++ src/utils/wallet.ts
@@ -1,8 +1,15 @@
 import { initializeConnector } from "@web3-react/core";
 import { MetaMask } from "@web3-react/metamask";
+import { Connector } from "@web3-react/types";
 
 export const [metaMask, Web3ReactHooks, Web3ReactStore] =
   initializeConnector<MetaMask>((actions) => new MetaMask({ actions }));
+
+export const connectors: [
+  Connector,
+  typeof Web3ReactHooks,
+  typeof Web3ReactStore
+][] = [[metaMask, Web3ReactHooks, Web3ReactStore]];
 
 export const connectWallet = () => metaMask.activate();
 
